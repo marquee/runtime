4edafd087403b55ff8cc9d93edca3885c80cd943
Alex Cabrera <souwecera@gmail.com>
2013-10-07 18:07:46 -0400

    `mark_safe` doesn't exist in Jinja
    Use Markup() instead

--- app/template_helpers.py
+++ app/template_helpers.py
@@ -288,5 +288,5 @@ def slugify(eval_ctx, value):
     """
     value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
     value = re.sub('[^\w\s-]', '', value).strip().lower()
-    return mark_safe(re.sub('[-\s]+', '-', value))
+    return Markup(re.sub('[-\s]+', '-', value))
 app.jinja_env.filters['slugify'] = slugify
