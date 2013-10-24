bfb41220b41ecc78f6140a750f5aaa071a0f3662
Alex Cabrera <alexandercabrera@me.com>
2013-10-24 02:19:42 -0400

    Use docopts for management command
    Usage: manage.py COMMAND [-p <port>] [--debug]
    
    Options:
        --debug     	watch for changes and reload app
    	-p <port>   	use an alternate port number
    
    Commands:
    	runserver		run the Flask application

--- manage.py
+++ manage.py
@@ -1,19 +1,110 @@
-from app 			  import app
-from app 		      import settings
+""" Usage: manage.py COMMAND [-p <port>] [--debug]
 
-from flask.ext.script import Manager
+Options:
+    --debug     watch for changes and reload app
+    -p <port>   use an alternate port number
+
+Commands:
+    runserver           run the Flask application
+
+"""
+
+from docopt import docopt
+
+from app import app
+from app import settings
+
+import logging
 import sys
 
-manager = Manager(app)
+# Colored Logging Formatter
+# =========================
+# Borrowed from http://mrqe.co/16uCGRh
 
-if settings.HYPERDRIVE:
-	from hyperdrive.commands 	  import manager as hypermanager
-	manager.add_command("hyperdrive", hypermanager)
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+RESET_SEQ   = "\033[0m"
+COLOR_SEQ   = "\033[1;%dm"
+BOLD_SEQ    = "\033[1m"
+COLORS      = {
+    'WARNING': YELLOW,
+    'INFO': WHITE,
+    'DEBUG': BLUE,
+    'CRITICAL': YELLOW,
+    'ERROR': RED,
+}
 
-if __name__ == '__main__':
 
-    if 'show_routes' in sys.argv:
-        for r in app.url_map.iter_rules():
-            print r
+def formatter_message(message, use_color = True):
+    if use_color:
+        message = message.replace("$RESET", RESET_SEQ).replace("$BOLD", BOLD_SEQ)
     else:
-    	manager.run()
+        message = message.replace("$RESET", "").replace("$BOLD", "")
+    return message
+
+class ColoredFormatter(logging.Formatter):
+    def __init__(self, msg, use_color = True):
+        logging.Formatter.__init__(self, msg)
+        self.use_color = use_color
+
+    def format(self, record):
+        levelname = record.levelname
+        if self.use_color and levelname in COLORS:
+            levelname_color = COLOR_SEQ % (30 + COLORS[levelname]) + levelname + RESET_SEQ
+            record.levelname = levelname_color
+        return logging.Formatter.format(self, record)
+
+class ColoredLogger(logging.Logger):
+    FORMAT = "[$BOLD%(name)-2s$RESET][%(levelname)-4s] %(message)s"
+
+    COLOR_FORMAT = formatter_message(FORMAT, True)
+    def __init__(self, name):
+        logging.Logger.__init__(self, name, logging.DEBUG)
+
+        color_formatter = ColoredFormatter(self.COLOR_FORMAT)
+
+        console = logging.StreamHandler()
+        console.setFormatter(color_formatter)
+
+        self.addHandler(console)
+        return
+
+logging.setLoggerClass(ColoredLogger)
+logger = logging.getLogger('runtime.manage')
+
+
+# If hyperdrive hasn't been initiated, don't use it
+try:
+    from hyperdrive.commands    import manager as hypermanager
+except ImportError:
+    settings.HYPERDRIVE = False
+
+AVAILABLE_COMMANDS = [
+    'runserver'
+]
+
+# Main program loop
+if __name__ == '__main__':
+    arguments = docopt(__doc__, help=True)
+    command   = arguments.get('COMMAND')
+    app_kwrds = {}
+
+    if command is None:
+        print __doc__
+        quit()
+
+    if command not in AVAILABLE_COMMANDS:
+        print "{0} is not an available command\n".format(command)
+        print __doc__
+        quit()
+
+    if not settings.HYPERDRIVE:
+        logger.warning('Hyperdrive not initiated')
+
+    if settings.HYPERDRIVE:
+        app_kwrds['hyperdrive'] = hypermanager
+
+    if command == 'runserver':
+        debug   = arguments.get('--debug')
+        port    = int(arguments.get('-p', settings.PORT))
+
+        app.run(port=port, debug=debug)
